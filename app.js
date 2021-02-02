2c03e05d237d0010b1dc28eea344414def0b76ed
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-02-02 16:09:43 +0100

    v4

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-async function dataHandler(data) {
-    let result = await data.json();
-    console.log(result);
-}
-
-function errorHandler(err) {
-    console.log(err);
-}
-
-let url = 'https://imdb8.p.rapidapi.com/title/auto-complete?q=back%20to%20the';
-let options = {
-    'method':'GET',
-    'headers': {
-        'x-rapidapi-key':'',
-        'x-rapidapi-host':'imdb8.p.rapidapi.com'
+function dataReceiver() {
+    async function dataHandler(data) {
+        let result = await data.json();
+        console.log(result);
+        for (let index = 0; index < result.d.length; index++) {
+            const movie = result.d[index];
+            let card = `
+            <div class="card">
+                <img src = "${movie.i.imageUrl}"
+            </div>
+            `
+            document.querySelector('body').insertAdjacentHTML('beforeend',card);
+        }
     }
-};
 
-fetch(url, options).then(dataHandler).catch(errorHandler);
+    function errorHandler(err) {
+        console.log(err);
+    }
+
+    let url = 'https://imdb8.p.rapidapi.com/title/auto-complete?q='+document.getElementById('search').value;
+    let options = {
+        'method':'GET',
+        'headers': {
+            'x-rapidapi-key':'',
+            'x-rapidapi-host':'imdb8.p.rapidapi.com'
+        }
+    };
+
+    fetch(url, options).then(dataHandler).catch(errorHandler);
+}
